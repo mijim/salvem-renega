363d928648baeda5c52c903a51417d167331d583
Miguel Jimenez Benajes <mijim@mijims-MacBook-Pro.local>
2020-05-17 17:50:37 +0200

    fix: deploy error

--- pages/multimedia/index.js
+++ pages/multimedia/index.js
@@ -7,9 +7,37 @@ import { translateCat } from '../../translations/cat';
 import { useEffect, useState } from 'react';
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
-import { urlNews } from './info_urls';
 
-
+const urlNews = [
+    {
+        title: 'La Renegà: Un observatorio marítimo en pleno litoral de Orpesa: ¿el nuevo Marina d’Or?',
+        link: 'https://www.publico.es/sociedad/renega-centro-interpretacion-mediterraneo-orpesa-nuevo-marina-d-or.html',
+        image: 'https://www.publico.es/uploads/2020/05/16/5ebff43ec3e60.jpeg',
+        description: 'Movimientos ecologistas denuncian que la construcción de un "observatorio marino" en una parcela urbana del litoral de Orpesa esconde como verdadera finalidad la resurrección de antiguos proyectos urbanísticos paralizados por la crisis.',
+        source: 'Público'
+    },
+    {
+        title: 'Compromís pide "total transparencia" sobre el proyecto del entorno de la Renegà',
+        link: 'https://castellonplaza.com/compromis-pide-total-transparencia-sobre-el-proyecto-del-entorno-de-la-renega',
+        image: 'https://castellonplaza.com/public/Image/2020/5/WhatsAppImage2020-05-13at12.09.11_NoticiaAmpliada.jpg',
+        description: '',
+        source: 'Castellón Plaza'
+    },
+    {
+        title: 'Polèmica per l’inici de les obres d’un centre interpretatiu prop de la Renegada, a Orpesa',
+        link: 'https://apuntmedia.es/va/noticies/societat/polemica-per-l-inici-de-les-obres-d-un-centre-interpretatiu-prop-de-la-renegada-a-orpesa',
+        image: 'https://statics.apuntmedia.es/news/IMG0000074968_1589220691.jpeg',
+        description: '“Salvem la Renegà” ha aconseguit més de 10.000 firmes per a evitar “l’urbanisme salvatge” als voltants del paratge natural.',
+        source: 'À Punt'
+    },
+    {
+        title: 'Más de 7.400 firmas recogidas para paralizar un proyecto en Bellver, junto a la Renegà',
+        link: 'https://cadenaser.com/emisora/2020/05/10/radio_castellon/1589114336_703730.html',
+        image: 'https://cadenaser00.epimg.net/ser/imagenes/2020/05/10/radio_castellon/1589114336_703730_1589114525_noticia_normal_recorte1.jpg',
+        description: 'Un proyecto defendido como medio ambiental y divulgativo, que modificará más de un kilómetro de paraje natural de calas y bosque mediterráneo',
+        source: 'Cadena SER'
+    }
+]
 export default function Multimedia() {
     const [language, setLanguage] = useState('es');
     const { t } = useTranslation();
